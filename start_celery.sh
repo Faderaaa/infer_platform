a8b86b2d8c2270c047d226cd35462f027e711300
Faderaaa <1654219417@qq.com>
2025-02-18 22:53:59 +0800

    推理平台onnx版本

--- start_celery.sh
+++
@@ -1,3 +0,0 @@
-nohup celery -A app.celery flower --address=127.0.0.1 --port=5566  > ./log/flower.log 2>&1 &
-sleep 2
-nohup celery -A app.celery  worker --loglevel=INFO  > ./log/celery.log 2>&1 &
--- stop_celery.sh
+++
@@ -1,1 +0,0 @@
-ps -ef | grep celery | grep -v 'grep' | awk '{print $2}' |xargs kill -9
